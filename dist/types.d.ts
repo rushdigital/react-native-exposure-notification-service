c96f9e47b2bc4b49876be1f4aa224f33c6684b4d
will <will@rushdigital.co.nz>
2021-01-25 11:01:51 +1300

    Dist

---
+++ dist/types.d.ts
@@ -0,0 +1,23 @@
+export interface TraceConfiguration {
+    exposureCheckInterval: number;
+    storeExposuresFor: number;
+}
+export declare enum PermissionStatus {
+    Unknown = "unknown",
+    NotAvailable = "not_available",
+    Allowed = "allowed",
+    NotAllowed = "not_allowed"
+}
+export interface PermissionDetails {
+    status: PermissionStatus.Unknown | PermissionStatus.NotAvailable | PermissionStatus.NotAllowed | PermissionStatus.Allowed;
+    internal?: any;
+}
+export interface ExposurePermissions {
+    exposure: PermissionDetails;
+    notifications: PermissionDetails;
+}
+export interface Version {
+    version: String;
+    build: String;
+    display: String;
+}

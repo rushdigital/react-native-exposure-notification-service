7d1bd63d6eebac8d24cf26503baa3beb1c403952
will <will@rushdigital.co.nz>
2021-02-25 20:37:35 +1300

    Add dist

---
+++ dist/types.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PermissionStatus = void 0;
+var PermissionStatus;
+(function (PermissionStatus) {
+    PermissionStatus["Unknown"] = "unknown";
+    PermissionStatus["NotAvailable"] = "not_available";
+    PermissionStatus["Allowed"] = "allowed";
+    PermissionStatus["NotAllowed"] = "not_allowed";
+})(PermissionStatus = exports.PermissionStatus || (exports.PermissionStatus = {}));
